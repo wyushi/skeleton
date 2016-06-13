aa69af4251fc34d1e17eba9a1624970ceea7c4a0
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-13 16:44:01 -0600

    add mongo and elastic-search to docker-compose

---
+++ dev-setup/dev-machine/clean.sh
@@ -0,0 +1,9 @@
+docker stop some-api
+docker rm some-api
+docker stop some-elastic
+docker rm  some-elastic
+docker stop some-redis
+docker rm some-redis
+docker stop some-mongo
+docker rm some-mongo
+
