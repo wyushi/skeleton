de2e811c3ff1726b5d4635aa727a9920f14c2904
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-13 13:23:09 -0600

    make mocha unit test run from tools folder

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -4,9 +4,12 @@ var gulp = require('gulp')
   , runSequence = require('run-sequence')
   , nodemon = require('gulp-nodemon')
   , plugins = require('gulp-load-plugins')()
-  , babel = require("gulp-babel");
+  , babel = require("gulp-babel")
+  , babelRegister = require('babel-core/register')
+  , mocha = require("gulp-mocha");
 
-var root = '../';
+var root = '../',
+    generatedFile = 'all.js'
 
 /**
  * Load a gulp task module
@@ -17,13 +20,22 @@ function getTask(task) {
   return require('./gulp-tasks/' + task)(gulp, plugins);
 }
 
-gulp.task("build", () => {
-  return gulp.src("../server-side/server.js")
+gulp.task('build', () => {
+  return gulp.src('../server-side/server.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("all.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("../server-side/dist"));
+    .pipe(concat(generatedFile))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('../server-side/dist'));
+});
+
+gulp.task('unit-test', () => {
+  return gulp.src(['../server-side/tests/**/*.js'])
+    .pipe(mocha({
+      compilers: {
+        js: babelRegister
+      }
+    }));
 });
 
 /**
