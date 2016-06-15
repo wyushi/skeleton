e8bf026ca36af97a8886ccdf8009cbf6fa73ffcc
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-15 17:09:45 -0600

    add a few server packeges

--- server-side/server.js
+++ server-side/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import logger from 'morgan';
+import methodOverride from 'method-override';
+import bodyParser from 'body-parser';
+
 
 var app = express();
+app.use(logger('combined'));
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('hello world!');
 });
 
 app.listen(3000);
-console.log('server starts listen on port 3000 ...');
+console.log('Server starts listening on port 3000 ...');
