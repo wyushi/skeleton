9488eef1f3b9141d15763ec62882985bfade1090
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-16 10:00:14 -0600

    add secure connection to express server
    change locations of ssl certs and keys

--- dev-setup/generate-ssl-certs.sh
+++ dev-setup/generate-ssl-certs.sh
@@ -1,7 +1,7 @@
 echo "Generating self-signed certificates ..."
-mkdir ./ssl/
-openssl genrsa -out ./ssl/key.pem -aes256 1024
-openssl req -new -key ./ssl/key.pem -out ./ssl/csr.pem
-openssl x509 -req -days 9999 -in ./ssl/csr.pem -signkey ./ssl/key.pem -out ./ssl/cert.pem
-rm ./ssl/csr.pem
-chmod 600 ./ssl/key.pem ./ssl/cert.pem
+mkdir ../server-side/ssl/
+openssl genrsa -out ../server-side/ssl/key.pem -aes256 1024
+openssl req -new -key ../server-side/ssl/key.pem -out ../server-side/ssl/csr.pem
+openssl x509 -req -days 9999 -in ../server-side/ssl/csr.pem -signkey ../server-side/ssl/key.pem -out ../server-side/ssl/cert.pem
+rm ../server-side/ssl/csr.pem
+chmod 600 ../server-side/ssl/key.pem ../server-side/ssl/cert.pem
--- server-side/server.js
+++ server-side/server.js
@@ -2,9 +2,18 @@ import express from 'express';
 import logger from 'morgan';
 import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 
 
-var app = express();
+const credentials = {
+        key: fs.readFileSync(__dirname + '/ssl/key.pem', 'utf8'),
+        cert: fs.readFileSync(__dirname + '/ssl/cert.pem', 'utf8'),
+        passphrase: '0000'
+      },
+      app = express();
+
 app.use(logger('combined'));
 app.use(methodOverride());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,5 +22,7 @@ app.get('/', (req, res) => {
   res.send('hello world!');
 });
 
-app.listen(3000);
+
+// http.createServer(app).listen(3000);
+https.createServer(credentials, app).listen(3000);
 console.log('Server starts listening on port 3000 ...');
