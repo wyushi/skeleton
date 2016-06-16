ab579a7f4a07140617f4da10ae7f24f75ba093de
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-16 17:02:59 -0600

    connect to mongo

--- server-side/server.js
+++ server-side/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import fs from 'fs';
 import http from 'http';
 import https from 'https';
+import mongoose from 'mongoose';
 
 
 const credentials = {
@@ -12,16 +13,20 @@ const credentials = {
         cert: fs.readFileSync(__dirname + '/ssl/cert.pem', 'utf8'),
         passphrase: '0000'
       },
+      mongo = {
+        host: 'mongo',
+        port: 27017,
+        name: 'dev'
+      },
       app = express();
 
 app.use(logger('combined'));
 app.use(methodOverride());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
-});
-
+// db connections
+mongoose.connection.on('error', console.error);
+mongoose.connect(`mongodb://${mongo.host}:${mongo.port}/${mongo.name}`);
 
 // http.createServer(app).listen(3000);
 https.createServer(credentials, app).listen(3000);
