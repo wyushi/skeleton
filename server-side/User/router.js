9cd9b2599797b08889416a4ce645fbba4dcb2cf5
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-17 17:11:36 -0600

    add api for activate user

--- server-side/User/router.js
+++ server-side/User/router.js
@@ -1,36 +1,66 @@
 import User from './model.js';
 import mailgun from 'mailgun-js';
 import { mailgun as gunConfig } from '../config.js';
+import { confirmMail } from './mail-generator.js';
+import { create as createCode, consume as consumeCode } from './activate-code.js';
 
 
 const route = '/users',
       gun = mailgun(gunConfig);
+
+function sendActivateCode(user, next) {
+  createCode(user._id).then((code) => {
+    gun.messages().send(confirmMail(user, code));
+  }).catch(next);
+}
 
 function attachTo(app) {
 
   app.get(route, (req, res, next) => {
     User.find().exec((error, users) => {
       if (error) { return next(error); }
-      res.status(200).send(users);
+      res.send(users);
     });
   });
 
   app.post(route, (req, res, next) => {
     User.create(req.body, (error, user) => {
       if (error) { return next(error); }
-      console.log(user.email);
-      var mail = {
-        from: 'Skeleton Server <mail@yushiwang.ca>',
-        to: user.email,
-        subject: 'Hello from Skeleton Server',
-        text: 'Happy to see this message.'
-      };
-      gun.messages().send(mail, (error, body) => {
-        if (error) { return next(error); }
-        else { console.log(body); }
-      })
+      sendActivateCode(user, next);
       res.send(user);
     });
+  });
+
+  app.get(route + '/:id', (req, res, next) => {
+    User.findOne({ _id: req.params.id })
+        .exec((error, user) => {
+          if (error) { return next(error); }
+          res.send(user);
+        });
+  });
+
+  app.get(route + '/:id/reactivate',(req, res, next) => {
+    User.findOne({ _id: req.params.id })
+        .exec((error, user) => {
+          if (error) { return next(error); }
+          sendActivateCode(user, next);
+          res.send(user);
+        });
+  });
+
+  app.get(route + '/:id/confirm/:code', (req, res, next) => {
+    consumeCode(req.params.id, req.params.code)
+      .then((code) => {
+        User.findOne({ _id: req.params.id })
+            .exec((error, user) => {
+              if (error) { return next(error); }
+              user.activate((error, user) => {
+                if (error) { return next(error); }
+                res.send(user);
+              });
+            });
+      })
+      .catch(next);
   });
 }
 
