fd11f9396b92a70513f85c93f2e7e44bd9e4b960
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-22 10:48:30 -0600

    update sample config

--- server-side/config.example.js
+++ server-side/config.example.js
@@ -4,12 +4,21 @@ import fs from 'fs';
 export const host = 'localhost';
 export const port = 3000;
 export const credentials = {
-                key: fs.readFileSync(__dirname + '/ssl/key.pem', 'utf8'),
-                cert: fs.readFileSync(__dirname + '/ssl/cert.pem', 'utf8'),
-                passphrase: '0000'
-              };
+  key: fs.readFileSync(__dirname + '/ssl/key.pem', 'utf8'),
+  cert: fs.readFileSync(__dirname + '/ssl/cert.pem', 'utf8'),
+  passphrase: '0000'
+};
+export const redis = {
+  host: 'redis',
+  port: 6379
+};
 export const mongo = {
-                host: 'mongo',
-                port: 27017,
-                name: 'dev'
-              };
+  host: 'mongo',
+  port: 27017,
+  name: 'dev'
+};
+export const mailgun = {
+  apiKey: '',
+  domain: ''
+};
+
